f0c26c2d16a87d605bff6a146d62e38454318702
Egor Mikhalchenko <gorniygor@gmail.com>
2017-06-23 20:26:27 +0300

    Add some tests.

--- app/src/androidTest/java/com/example/gor/myhomies3/MockitoTest.java
+++ app/src/androidTest/java/com/example/gor/myhomies3/MockitoTest.java
@@ -1,8 +1,20 @@
 package com.example.gor.myhomies3;
 
+import java.util.List;
+import static org.mockito.Mockito.*;
+
 /**
  * Created by Gor on 23.06.2017.
  */
 
 public class MockitoTest {
+    List mockedList = mock(List.class);
+
+    // using mock object - it does not throw any "unexpected interaction" exception
+    mockedList.add("one");
+    mockedList.clear();
+
+    // selective, explicit, highly readable verification
+    verify(mockedList).add("one");
+    verify(mockedList).clear();
 }
